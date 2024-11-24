dcfe8d6f5dbdd58bb78a3c3d966a8b830b883ea2
PSSaparov <saparov.petr@gmail.com>
2024-11-24 13:04:34 +0300

    welcome component edited

--- src/components/welcome-component/WelcomeComponent.tsx
+++ src/components/welcome-component/WelcomeComponent.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { FC } from 'react';
+
+import { Link } from 'react-router-dom';
 
 import styles from './WelcomeComponent.module.css';
 import { RoundButton } from '../RoundButton/RoundButton';
@@ -6,11 +9,32 @@ import { markTheFirstStart } from '@//utils/localstorage-functionality';
 
 const WelcomeComponent: FC = () => (
   <div className={styles.welcomeContainer}>
-    <div>👋</div>
-    <div>Welcome to the GitTreiner!</div>
+    <div>👋 Салют!</div>
     <div>
-      You can brush up words and edit Markdown notes for them from your GitHub.
+      Это приложение для закрепления словарного запаса английского языка.
+      Основано на идее повторения слов по заметкам (markdown-файлам) из твоего
+      личного репозитория на GitHub.
     </div>
+    <div>Как это работает:</div>
+    <div>1. Зарегистрируйся и войди на GitHub;</div>
+    <div>
+      2. Форкни себе этот{' '}
+      <Link to={'https://github.com/SaparovPetr/emptyMdFilesForFork'}>
+        <span className={styles.linkword}>репозиторий</span>
+      </Link>{' '}
+      и укажи данные о нем и о своем профиле в настройках приложения;
+    </div>
+    <div>
+      3. Заполняй заметки к подзабытым словам, используя встроенный AI-сервис*.
+    </div>
+    <div className={styles.footnote}>
+      * Ввиду отсутствия у меня бюджета на сервер, модель работает не всегда и
+      активируется по личному запросу в{' '}
+      <Link to={'https://t.me/SaparovPetr'}>
+        <span className={styles.linkword}>телеграмм.</span>
+      </Link>
+    </div>
+
     <RoundButton onClickFunc={markTheFirstStart} disabled={false}>
       →
     </RoundButton>
