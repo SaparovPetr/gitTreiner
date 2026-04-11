5aff91618c8718f2e1d201c659e6861e76e507bb
sps <saparov.petr@gmail.com>
2026-04-11 14:25:25 +0300

    mode slice removed

--- src/services/root-reducer.ts
+++ src/services/root-reducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import { mdSlice } from './slices/md-slice';
-import { modeSlice } from './slices/mode-slice';
 import { wordsSlice } from './slices/words-slice';
 
 const rootReducer = combineReducers({
   [wordsSlice.name]: wordsSlice.reducer,
-  [modeSlice.name]: modeSlice.reducer,
   [mdSlice.name]: mdSlice.reducer
 });
 
