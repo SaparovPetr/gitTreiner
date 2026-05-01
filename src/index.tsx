15bb8b409addc6d62cd3ed99ad5fd3902739181d
sps <saparov.petr@gmail.com>
2026-05-01 17:46:31 +0300

    RTK dependancy removed

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import './index.css';
 import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App/App';
